70111a16ad1b5300b7e90de6840715a04659fffc
OmarAdel333332 <omaradel.33333332@gmail.com>
2025-01-09 21:35:18 +0200

    full modify

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ let navScrollA = document.querySelectorAll('.nav-link');
 let navHeight = navScroll.offsetHeight;
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('.nav-link');
-let scroll = window.scrollY;
 window.addEventListener('scroll', function() {
     scroll = window.scrollY;
     sections.forEach((section, index) => {
@@ -27,7 +26,7 @@ for (let i = 0; i < links.length; i++) {
     });
 }
 window.addEventListener('scroll', function() {
-if (window.scrollY > 0) {
+if (scroll > 0) {
     navScroll.classList.add('bg-light');
     navScroll.style = 'box-shadow: 0 2px 4px rgba(0,0,0,.1)';
     links.forEach(link => {
@@ -43,7 +42,7 @@ if (window.scrollY > 0) {
 let scrollUp = document.querySelector('.scroll-up');
 let about = document.querySelector('.about');
 window.addEventListener('scroll', function() {
-    if (window.scrollY > about.offsetTop) {
+    if (scroll > about.offsetTop) {
         scrollUp.style = 'display: flex';
     } else {
         scrollUp.style = 'display: none';
@@ -52,5 +51,21 @@ window.addEventListener('scroll', function() {
 scrollUp.addEventListener('click', function() {
     window.scrollTo(0,0);
 });
-
+let inputs = document.querySelectorAll('.form-control');
+let invalids = document.querySelectorAll('.invalid-label');
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('blur', function() {
+        if (inputs[i].value == '') {
+            inputs[i].classList.add('is-invalid');
+            inputs[i].classList.remove('is-valid');
+            inputs[i].classList.remove('mb-3');
+            invalids[i].style = 'display: block';
+        } else {
+            invalids[i].style = 'display: none';
+            inputs[i].classList.remove('is-invalid');
+            inputs[i].classList.add('is-valid');
+            inputs[i].classList.add('mb-3');
+        }
+    });
+}
 
